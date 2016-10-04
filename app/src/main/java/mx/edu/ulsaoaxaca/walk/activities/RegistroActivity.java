8a56373033f85d44c9e6f6a0486ede63d9669229
alosocumo <sol_alaska@hotmail.com>
2016-10-04 18:31:59 -0500

    nuevos cambios

---
+++ app/src/main/java/mx/edu/ulsaoaxaca/walk/activities/RegistroActivity.java
@@ -0,0 +1,15 @@
+package mx.edu.ulsaoaxaca.walk.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.edu.ulsaoaxaca.walk.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
