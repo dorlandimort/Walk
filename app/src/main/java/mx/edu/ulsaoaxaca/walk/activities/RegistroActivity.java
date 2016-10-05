8070b14637e4daf408d3d62b38cd222c9e7d8702
alosocumo <sol_alaska@hotmail.com>
2016-10-04 19:32:42 -0500

    Nuevos cambios2

--- app/src/main/java/mx/edu/ulsaoaxaca/walk/activities/RegistroActivity.java
+++ app/src/main/java/mx/edu/ulsaoaxaca/walk/activities/RegistroActivity.java
@@ -1,15 +1,171 @@
 package mx.edu.ulsaoaxaca.walk.activities;
 
+import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import mx.edu.ulsaoaxaca.walk.R;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    private static String BASE_URL = "http://192.168.1.67:8080/server/";
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Registro Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://mx.edu.ulsaoaxaca.walk.activities/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Registro Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://mx.edu.ulsaoaxaca.walk.activities/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public class Sincronizar extends AsyncTask<Void, String, String> {
+
+
+            private  String nombre;
+            private String usuario;
+            private String password;
+            private  String peso;
+            private  String estatura;
+            private  String edad;
+            private  String sexo;
+
+        Sincronizar(String usuario, String password, String nombre,String peso, String estatura,String edad, String sexo) {
+            this.nombre= nombre;
+            this.usuario = usuario;
+            this.password = password;
+            this.peso = peso;
+            this.estatura = estatura;
+            this.edad = edad;
+            this.sexo = sexo;
+
+        }
+
+            @Override
+            protected String doInBackground (Void...params){
+            StringBuilder result = new StringBuilder();
+
+            try {
+                URL url = new URL(BASE_URL + "persona");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "plain/text");
+                conn.setUseCaches(false);
+                conn.setConnectTimeout(10000);
+                conn.setReadTimeout(10000);
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                JSONObject json = new JSONObject();
+                json.put("nombre", nombre);
+                json.put("sexo",sexo);
+                json.put("usuario", usuario);
+                json.put("password", password);
+                json.put("peso",peso);
+                json.put("estatura",estatura);
+                json.put("edad",edad);
+
+                DataOutputStream output;
+
+                output = new DataOutputStream(conn.getOutputStream());
+                output.writeBytes(json.toString());
+                output.flush();
+                output.close();
+
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    reader.close();
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return result.toString();
+        }
+        }
+    }
+
